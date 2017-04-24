20cd020d66d7b8dac8778d3ceedbc6df1c5644d6
apb973 <apb973@users.noreply.github.com>
2017-04-24 13:30:50 -0500

    added attributes to Station

--- Search Outline/Station.java
+++ Search Outline/Station.java
@@ -4,6 +4,8 @@ public class Station {
 	private Restaurants restaurants;
 	private double price;
 	private Location location;
+	private String name;
+	private String address;
 	
 	public Station(){
 		bathroom = null;
@@ -14,6 +16,14 @@ public class Station {
 	
 	public Bathroom getBathroom(){
 		return bathroom;
+	}
+	
+	public String getName(){
+		return name;
+	}
+	
+	public String getAddress(){
+		return address;
 	}
 	
 	public Restaurants getRestaurants(){
@@ -32,15 +42,23 @@ public class Station {
 		bathroom = restroom;
 	}
 	
-	public void getRestaurants(Restaurants toChange){
+	public void setRestaurants(Restaurants toChange){
 		restaurants = toChange;
 	}
 	
-	public void getPrice(double toChange){
+	public void setPrice(double toChange){
 		price = toChange;
 	}
 	
-	public void getLocation(Location toChange){
+	public void setLocation(Location toChange){
 		location = toChange;
+	}
+	
+	public void setName(String str){
+		name = str;
+	}
+	
+	public void setAddress(String str){
+		name = str;
 	}
 }
