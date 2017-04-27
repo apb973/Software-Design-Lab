48b10ee83b9c33702e504022a502fe9148f1a2fa
apb973 <apb973@users.noreply.github.com>
2017-04-27 16:22:07 -0500

    added actual search implementation

--- Search Outline/BathroomSearch.java
+++ Search Outline/BathroomSearch.java
@@ -1,31 +1,24 @@
 
 public class BathroomSearch extends Queryable implements Searchable{
 	
-	private int radius = 5;
-	
 	public Results search(Location location){
-		Results results;
-		boolean allHaveBathrooms;
+		Results finalResults = initialQuery(location);
+		Station finalResult;
+		int finalSize = finalResults.size();
 		
-		do{
-			allHaveBathrooms = true;
-			radius += 5;
-			results = googleStationSearch(location, radius);			//if we store stations and their info in a database,
-			for(int i = 0; i < results.size(); i++){						//can do database search first, then google search if not enough found
-				if(results.getStation(i).getBathroomRating() == null){
-					allHaveBathrooms = false;
-					break;
-				}
-			}
-		}while(!allHaveBathrooms && results.size() == 10);
-		radius = 5;
+		//TODO: addBathrooms
+		finalResults = sort(finalResults);
 		
-		//if we store stations and their info in a database, can get rid of set functions
-		setStationPrices(results);
-		setRestaurants(results);
+		for(int i = 5; i < finalSize; i++){
+			finalResults.removeStation(i);
+		}
 		
-		results = sort(results);
-		return results;
+		finalSize = finalResults.size();
+		for(int i = 0; i < finalSize; i++){
+			finalResult = finalResults.getStation(i);
+			finalResult.setRestaurants(googleRestaurants(finalResult.getLocation()));
+		}
+		return finalResults;
 	}
 	
 	public Results sort(Results results){
