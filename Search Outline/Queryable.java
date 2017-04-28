0fd0ce431ec47d1a58c5fd09c0473ef1e4813a7e
apb973 <apb973@users.noreply.github.com>
2017-04-28 01:44:42 -0500

    fixed RestaurantQuery

--- Search Outline/Queryable.java
+++ Search Outline/Queryable.java
@@ -105,7 +105,7 @@ public class Queryable {
 	            names = new Restaurants();
 	            int size = nameElements.size();
 	            
-	            for (int i = 0; ((i < size) || (i < 5)); i++) {
+	            for (int i = 0; ((i < size) && (i < 5)); i++) {
 	            	names.addRestaurant(nameElements.get(i).text());
 	            }
 	        } catch (IOException e) {
