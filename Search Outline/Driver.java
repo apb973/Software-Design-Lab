1860d45a64b03ba2111e099eae4c03c9369bac07
apb973 <apb973@users.noreply.github.com>
2017-04-28 02:44:17 -0500

    Delete Driver.java

--- Search Outline/Driver.java
+++
@@ -1,8 +0,0 @@
-
-public class Driver {
-    public static void main(String[] args) {
-		NoFilterSearch test = new NoFilterSearch();
-		Location holder = new Location(30.307182,-97.755996);
-		test.search(holder);
-}
-}
