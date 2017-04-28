d06e59c1a92a6844c418f56f23db7062b5ff0691
104102lim <104102lim@naver.com>
2017-04-28 09:43:06 -0500

    Branch for Lim

---
+++ Lim/src/NoFilterSearch.java
@@ -0,0 +1,28 @@
+
+public class NoFilterSearch extends Queryable implements Searchable{
+	
+	
+	public Results search(Location location){
+		Results finalResults = initialQuery(location);
+		Station finalResult;
+		int finalSize = finalResults.size();
+		
+		for(int i = 5; i < finalSize; i++){
+			finalResults.removeStation(i);
+		}
+		
+		finalSize = finalResults.size();
+		for(int i = 0; i < finalSize; i++){
+			finalResult = finalResults.getStation(i);
+			finalResult.setRestaurants(googleRestaurants(finalResult.getLocation()));
+		}
+		return finalResults;
+	}
+	
+	public Results sort(Results results){
+		//put closest station first
+		
+		//should already be in order
+		return results;
+	}
+}
